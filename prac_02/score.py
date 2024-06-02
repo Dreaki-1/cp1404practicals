a71f68b42818b577a7c69e347b18230a8201a4c8
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-06-02 16:47:26 +1000

    Completed broken_score.py

--- prac_02/score.py
+++ prac_02/score.py
@@ -12,3 +12,32 @@ elif 50 <= score < 90:
     print("Passable")
 elif score < 100:
     print("Excellent")
+
+
+
+"""
+CP1404/CP5632 - Practical 02
+Broken program to determine score status
+"""
+
+
+def main():
+    """Get a user input and display score status"""
+    score = float(input("Enter score: "))
+    status = determine_score_status(score)
+    print(status)
+
+
+def determine_score_status(score):
+    """Take user's score and determine the status"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
