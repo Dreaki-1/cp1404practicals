190e984a5807a5589f6ea029c6ee95d80bb8cee0
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-06-02 15:43:01 +1000

    Add docstrings to each functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,3 +1,22 @@
-password = input("Enter your password: ")
-password = '*' * len(password)
-print(password)
+"""password_stars.py"""
+
+
+def main():
+    """This program will get a password and print it out in asterisks"""
+    password = get_password()
+    print_in_asterisks(password)
+
+
+def get_password():
+    """Get a password from the user"""
+    password = input("Enter your password: ")
+    return password
+
+
+def print_in_asterisks(prompt):
+    """Print out input as asterisks"""
+    prompt = '*' * len(prompt)
+    print(prompt)
+
+
+main()
