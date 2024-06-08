e89d61a656e68246622e04f2abbf9dc2f6c348f9
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-06-08 16:28:53 +1000

    Complete randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+"""
+Line 1
+Smallest: 5
+Largest: 20
+
+Line 2
+Smallest: 3
+Largest: 9
+Could line 2 produce 4?: NO
+
+Line 3
+Smallest: 2.5
+Largest: 5.5
+# Alot of possible outcomes since many are alot of decimal points
+"""
+
+import random
+
+print(random.randint(1,100))
