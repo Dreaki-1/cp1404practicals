9c66525505f28eec441f54dfe1205a52e5a32f43
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-06-20 21:56:25 +1000

    Add LBYL and EAFP methods of state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,29 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+"""LBYL"""
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in CODE_TO_NAME:
+        print(state, "is", CODE_TO_NAME[state])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
+"""EAFP"""
+while True:
+    try:
+        state = input("Enter short state: ").upper()
+        print(f"{state} is {CODE_TO_NAME[state]}")
+    except KeyError:
+        print("Invalid state")
+    else:
+        print("Goodbye")
+        break
+
+"""For part 4."""
+for state_abbreviation, state_name in CODE_TO_NAME.items():
+    print(f"{state_abbreviation:3} is {state_name}")
