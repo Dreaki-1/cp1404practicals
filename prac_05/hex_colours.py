8cbc6cbfecd438d544cdb78a61757a212e2a2795
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-06-23 15:45:50 +1000

    complete exercise hex_colours.py prac 05

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+COLOUR_NAMES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "	#f0f8ff",
+                "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Beige": "#f5f5dc",
+                "Champagne": "#f7e7ce", "Charcoal": "#36454f", "Cobalt": "#0047ab", "Coffee": "#6f4e37"}
+valid_colours = {name.lower(): colour.lower() for name, colour in COLOUR_NAMES.items()}
+
+choice = input("Valid Colours:").lower()
+while choice not in valid_colours:
+    print("Please enter a valid colour")
+    choice = input("Valid Colours:").lower()
+else:
+    print(f"The color code for {choice} is {valid_colours[choice]}")
